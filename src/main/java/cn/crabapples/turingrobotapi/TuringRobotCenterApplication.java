9f7b7741150d2a93f988f3ac5b7918e5ed2746d3
MiBook Deepin <crabapples.cn@gmail.com>
2021-09-02 11:45:11 +0800

    rename

--- src/main/java/cn/crabapples/turingrobotapi/TuringRobotCenterApplication.java
+++ src/main/java/cn/crabapples/turingrobotapi/TuringRobotCenterApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class TuringRobotApiApplication {
+public class TuringRobotCenterApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TuringRobotApiApplication.class, args);
+        SpringApplication.run(TuringRobotCenterApplication.class, args);
     }
 
 }
--- src/main/java/cn/crabapples/turingrobotapi/controller/TuringController.java
+++ src/main/java/cn/crabapples/turingrobotapi/controller/TuringController.java
@@ -1,5 +1,7 @@
 package cn.crabapples.turingrobotapi.controller;
 
+import cn.crabapples.turingrobotapi.utils.TuringApiUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/truing")
 public class TuringController {
+    private final TuringApiUtils turingApiUtils;
+
+    public TuringController(TuringApiUtils turingApiUtils) {
+        this.turingApiUtils = turingApiUtils;
+    }
+
     @GetMapping("/get/text")
     public String getText() {
-        return "demo";
+        String message = turingApiUtils.sendToTuringApi("哈哈哈");
+        return message;
     }
 }
